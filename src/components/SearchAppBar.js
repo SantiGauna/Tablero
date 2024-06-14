9181d3a4e8813407b0479c61e9fc742c477e7d8e
Joaquín <lazarojoaquin1990@gmail.com>
2024-06-14 17:53:09 -0300

    sEGUNDA

--- src/components/SearchAppBar.js
+++ src/components/SearchAppBar.js
@@ -30,12 +30,14 @@ const SearchAppBar = ({ searchTerm, setSearchTerm }) => {
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           SG Repuestos
         </Typography>
+        <div img style={{maxHeight: 40, float: 'left'}} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPkv0zUZZNU06PAIY4j8LcY40sZw5oVXmiaI7ZhiQOudPJtO4sgfe8fbqHoubKFaP1c-s&usqp=CAU' />
+        <div/>
         <div style={{ position: 'relative', borderRadius: 4, backgroundColor: alpha('#ffffff', 0.15) }}>
           <div style={{ position: 'absolute', display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%', pointerEvents: 'none', padding: '0 8px' }}>
             <SearchIcon />
           </div>
           <InputBase
-            placeholder="Buscar…"
+            placeholder="Buscar…"s
             inputProps={{ 'aria-label': 'search' }}
             value={searchTerm}
             onChange={handleSearchChange}
